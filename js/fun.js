4e382d536af807a1333c33fd402bc5ab1e2cf302
Konstantin Nochevny <139129282+kNoCoding@users.noreply.github.com>
2023-11-22 08:37:00 +0200

    initial commit

---
+++ js/fun.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const TICK = new Audio("audio/Tick-Made-with-Clipchamp.wav")
+const CLICK = new Audio("audio/Click-Made-with-Clipchamp.wav")
+const CLICKBOING = new Audio("audio/Click-Boing-Made-with-Clipchamp.wav")
+const HOOK = new Audio("audio/Hook-Made-with-Clipchamp.wav")
+const WEBS = new Audio("audio/Webs-Made-with-Clipchamp.wav")
+const TADA = new Audio("audio/Tada-Made-with-Clipchamp.wav")
+const PULSE = new Audio("audio/Pulse-Made-with-Clipchamp.wav")
+
+
+
+function playTick() {
+    TICK.play()
+}
+
+function playClick() {
+    CLICK.play()
+}
+
+function playClickBoing() {
+    CLICKBOING.play()
+}
+function playHook() {
+    HOOK.play()
+}
+function playWebs() {
+    WEBS.play()
+}
+function playTada() {
+    TADA.play()
+}
+function playPulse() {
+    PULSE.play()
+}
+
+// click boing: 1:01:08
+// fa - heart
+// fa - arrow - up - from - bracket
